c7d3cfa9e8f52219ce016d42020530632fa60a7f
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-01-02 19:46:40 +0500

    Second commit

--- app.py
+++ app.py
@@ -4,10 +4,64 @@ import json
 from BookModel import *
 from settings import *
 
+import jwt
+
+import datetime
+
+from UserModel import User
+
+from functools import wraps
+
+books = Book.get_all_books()
+
+DEFAULT_PAGE_LIMIT = 3
+
+app.config['SECRET_KEY'] = 'hello'
+
+@app.route('/login', methods=['POST'])
+def get_token():
+	request_data = request.get_json()
+	username = str(request_data['username'])
+	password = str(request_data['password'])
+
+	match = User.username_password_match(username, password)
+
+	if match:
+		expiration_date = datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
+		token = jwt.encode({'exp' : expiration_date}, app.config['SECRET_KEY'], algorithm='HS256')
+		return token
+	else:
+		return Response('', status=401, mimetype='application/json')
+
+# GET /books/page/<int:page_number>
+#/books/page/1?limit=100
+@app.route('/books/page/<int:page_number>')
+def get_pagginated_books(page_number):
+	print(type(request.args.get('limit')))
+	LIMIT = request.args.get('limit', DEFAULT_PAGE_LIMIT, int)
+	startIndex = page_number * LIMIT - LIMIT
+	endIndex = page_number * LIMIT
+	print(statIndex)
+	print(endIndex)
+	return jsonify({'books' : books[startIndex:endIndex]})
+
+def token_required(f):
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		token = request.args.get('token')
+		try:
+			jwt.decode(token, app.config['SECRET_KEY'])
+			return f(*args, **kwargs)
+		except:
+			return jsonify({'error' : 'Need a valid token to view this page'}), 401
+	return wrapper
+
+
 
 
 
 @app.route('/books')
+
 def get_books():
 	return jsonify({'books': Book.get_all_books()})
 
@@ -15,6 +69,7 @@ def get_books():
 
 
 @app.route('/books', methods=['POST'])
+@token_required
 def add_book():
 
 	request_data = request.get_json()
@@ -59,6 +114,7 @@ def valid_put_request_data(request_data):
 
 # PUT
 @app.route('/books/<int:isbn>', methods=['PUT'])
+@token_required
 def replace_book(isbn):
 	request_data = request.get_json()
 
@@ -80,6 +136,7 @@ def replace_book(isbn):
 	return response
 
 @app.route('/books/<int:isbn>', methods=['PATCH'])
+@token_required
 def update_book(isbn):
 	request_data = request.get_json()
 	
@@ -97,6 +154,7 @@ def update_book(isbn):
 
 
 @app.route('/books/<int:isbn>', methods=['DELETE'])
+@token_required
 def delete_book(isbn):
 	if(Book.delete_book(isbn)):
 		response = Response("", status=204)
